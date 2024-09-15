e665ce51c5fed6553afdd0ee2d6770c87dc9a8b1
Matgriv <groblovroum@gmail.com>
2024-09-15 18:33:30 +0200

    nettoyage code

--- intro dev/Td1/td1.java
+++ intro dev/Td1/td1.java
@@ -2,37 +2,31 @@ package Td1;
 
 public class td1{
     public static void main(String[] args){
-        Ut.afficherSL("feur");
-        exo7_2();
-    }
-
-    static void template(){
-        Ut.afficherSL("jsp");
+        System.out.println("ca arrive fort");
+        exo7_2(); //nom de l'exo a lancer
     }
 
     static void exo4(){
-        Ut.afficherSL("saisissez un entier:");
+        System.out.println("saisissez un entier:");
         int x = Ut.saisirEntier();
         for (int i = 0; i < 10; i++){
             x+=1;
-            Ut.afficherSL(x);
+            System.out.println(x);
         }
     }
 
     static void exo5_1(){
-        Ut.afficherSL("Entrez le nombre d'entiers:");
+        System.out.println("Entrez le nombre d'entiers:");
         int n = Ut.saisirEntier();
         int z = 0;
         int somme = 0;
         while (z < n){
             z += 1;
-            Ut.afficher("entrez l'entier n°");
-            Ut.afficherSL(z);
+            System.out.println("entrez l'entier n°"+z);
             int x = Ut.saisirEntier();
             somme += x;
         }
-        Ut.afficher("leur somme est de:");
-        Ut.afficherSL(somme);
+        System.out.println("Leur somme est de:"+somme);
 
     }
 
@@ -42,47 +36,40 @@ public class td1{
         int somme = 0;
 
         while (val){
-            Ut.afficherSL("Entrez un entier >= 0");
+            System.out.println("Entrez un entier >= 0");
             int x = Ut.saisirEntier();
             if (x>0){
                 counter += 1;
                 somme += x;
             }
             else if (x<0){
-                Ut.afficherSL("t con ou quoi chef");
+                System.out.println("t con ou quoi chef");
             }
             if (x==0){
                 val=false;
             }
         }
 
-        Ut.afficher("vous avez entré :");
-        Ut.afficher(counter);
-        Ut.afficherSL(" entiers");
-
-        Ut.afficher("leur somme est de :");
-        Ut.afficherSL(somme);
+        System.out.println("Vous avez entré:"+counter+"entiers");
+        System.out.println("leur somme est de:"+somme);
     }
 
     static void exo6(){
-        Ut.afficher("entrez un nombre n de personnes:");
+        System.out.println("entrez un nombre n de personnes:");
         int nbr = Ut.saisirEntier();
         int somme = 0;
         int count = 0;
         for (int i = 0; i<nbr; i++){
-            Ut.afficher("entrez l'age de la personne n°");
-            Ut.afficher(i+1);
-            Ut.afficher(":");
+            System.out.println("entrez l'âge de la personne n°"+(i+1)+":");
             int age = Ut.saisirEntier();
             somme += age;
             count += 1;
         }
-        Ut.afficher("la moyennes des âges est de: ");
-        Ut.afficherSL(somme/count);
+        System.out.println("La moyenne des âges est de:"+ (somme/count));
     }
 
     static void exo7_1(){
-        Ut.afficher("entrez un entier >= 0: ");
+        System.out.println("entrez un entier >= 0: ");
         int n = Ut.saisirEntier();
         if (n <= 0) {
             System.out.println("L'entier doit être strictement positif.");
@@ -92,14 +79,11 @@ public class td1{
         for (int i = 2; i<=n; i++){
             U = U + i;
         }
-        Ut.afficher("U");
-        Ut.afficher(n);
-        Ut.afficher("=");
-        Ut.afficher(U);
+        System.out.println("U"+n+"="+U);
     }
 
     static void exo7_2(){
-        Ut.afficher("entrez un entier >= 0: ");
+        System.out.println("entrez un entier >= 0: ");
         int n = Ut.saisirEntier();
         if (n <= 0) {
             System.out.println("L'entier doit être strictement positif.");
@@ -107,6 +91,7 @@ public class td1{
         }
         if (n == 1 || n == 2) {
             System.out.println("Le terme U" + n + " de la suite de Fibonacci est : 1");
+            return;
         }
         int U1 = 1, U2 = 1;
         int Un = 0;
@@ -118,6 +103,5 @@ public class td1{
         }
         System.out.println("U"+n+"="+Un);
     }
-
 
 }
