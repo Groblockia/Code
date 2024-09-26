3b799826852d7d38e8a52cb3b728acdf5d9400c6
Matgriv <groblovroum@gmail.com>
2024-09-26 16:39:34 +0200

    dev td3 exo 7 fini

--- intro dev/Td3/Exercice_7.java
+++ intro dev/Td3/Exercice_7.java
@@ -2,7 +2,25 @@ package Td3;
 
 public class Exercice_7 {
     public static void main(String[] args){
-        return;
+        System.out.print("saissez un entier:");
+        System.out.print("On a "+NbTrianglesRectangles(Ut.saisirEntier())+" triangles rectangles");
+    }
+
+    static int NbTrianglesRectangles(int p){
+        double z = 0;
+        int counter = 0;
+        for (double x = 0; x < p; x++) {
+            for (double y = 0; y < p; y++) {
+                z = Math.sqrt(((x*x)+(y*y)));
+                if ( x+y+z < p && ( x%1==0 && y%1==0 && z%1==0 )) {
+                    if ( (x>0&&y>0&&z>0) && (x>y)){
+                        counter+=1;
+                        System.out.println("le triangle de cotés a:"+x+" b:"+y+" h:"+z+" est un triangle rectangle entier");
+                    }
+                }
+            }
+        }
+        return counter;
     }
 
 }
