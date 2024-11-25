36fea5dc34a47f0f4cf6ef6493fcf2f3db5a6b49
Matéo Griveau <mateo.griveau01@etu.umontpellier.fr>
2024-11-25 16:31:48 +0100

    td8 MainCourse presque fini

--- intro dev/Td8/Course.java
+++ intro dev/Td8/Course.java
@@ -1,14 +1,17 @@
 package Td8;
 
 public class Course {
-    Voiture voit1;
-    Voiture voit2;
-    int longueur_piste;
+    public Voiture voit1;
+    public Voiture voit2;
+    public static int longueur_piste;
+    public static int k;
 
-    public Course (Voiture v1, Voiture v2, int long_piste){
+
+    public Course (Voiture v1, Voiture v2, int long_piste, int fois){
         this.voit1 = v1;
         this.voit2 = v2;
         this.longueur_piste = long_piste;
+        this.k = fois;
     }
 
     public String toString(){
@@ -19,25 +22,25 @@ public class Course {
         voit1.auDepart();
         voit2.auDepart();
 
-        while ( !voit1.depasse(longueur_piste) && !voit2.depasse(longueur_piste)){
+        while ( !voit1.depasse() && !voit2.depasse()){
             if (Ut.randomMinMax(1, 2) == 1){ voit1.avance();}
             else { voit2.avance();}
             System.out.print(voit1.toString2());
             System.out.print(voit2.toString2());
-            Ut.pause(250);
+            Ut.pause(150);
             Ut.clearConsole();
         }
 
-        if (voit1.depasse(longueur_piste)){ return voit1;}
+        if (voit1.depasse()){ return voit1;}
         else { return voit2;}
     }
 
 
     public static void main(String args[]){
-        Voiture v1 = new Voiture("2mahdi", 4);
-        Voiture v2 = new Voiture("1mateo", 4);
-        Course c = new Course(v1,v2,50);
-        System.out.println("le gagnant est: " +c.deroulement().leNom());
+        //Voiture v1 = new Voiture("2mahdi", 4);
+        //Voiture v2 = new Voiture("1mateo", 4);
+        //Course c = new Course(v1,v2,50);
+        //System.out.println("le gagnant est: " +c.deroulement().leNom());
     }
 
 }
--- intro dev/Td8/Voiture.java
+++ intro dev/Td8/Voiture.java
@@ -5,6 +5,8 @@ public class Voiture {
     private String nom;
     private int vitesse;
     public int position;
+    private Boolean sens = true;
+    private int count = 0;
 
 
 
@@ -34,12 +36,59 @@ public class Voiture {
         return this.nom;
     }
 
-    public boolean depasse(int limite){
-        return this.position >= limite;
+    public boolean depasse(){
+        if (sens){ 
+            return this.position > Course.longueur_piste;
+        }
+        else {
+            return this.position < 0;
+        }
+        
     }
 
     public void avance(){
-        this.position += this.vitesse;
+        int longueur = Course.longueur_piste;
+        int k = Course.k;
+        if (sens){
+            if ( (this.position + this.vitesse) > longueur ){
+                this.count += 1;
+                if (this.count == k){
+                    this.position += this.vitesse;
+                    return;
+                }
+                else{
+                    int x = longueur -= this.position;
+                    this.position += x;
+                    this.position -= (this.vitesse - x);
+                    this.sens = !this.sens;
+                }
+            }
+            else{
+                this.position += this.vitesse;
+            }
+        }
+        else{
+            if ( (this.position - this.vitesse) < 0 ){
+                this.count += 1;
+                if (this.count == k){
+                    this.position -= this.vitesse;
+                    return;
+                }
+                else{
+                    int x = 0 + this.position;
+                    this.position -= x;
+                    this.position += (this.vitesse - x);
+                    this.sens = !this.sens;
+                }
+            }
+            else{
+                this.position -= this.vitesse;
+            }
+        }
+
+        
+
+
     }
 
     public void auDepart(){
